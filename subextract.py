78969f434d1a45d41ec91dbf8c1fda188ba773b6
disrupted <hi@salomonpopp.me>
2021-12-26 16:57:22 +0100

    Init subextract script

---
+++ subextract.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import subprocess
+import sys
+from pathlib import Path
+
+import yaml
+
+parser = argparse.ArgumentParser(description="SubExtract")
+parser.add_argument(
+    "file",
+    metavar="F",
+    type=argparse.FileType("r"),
+    help="the .mkv video file",
+)
+
+args = parser.parse_args()
+path = Path(args.file.name)
+if path.stem not in ".mkv":
+    print(f"wrong file extension {path.stem}")
+    sys.exit(1)
+
+
+def mkvextract(track_id, out_filename):
+    mkvextract_args = [
+        "mkvextract",
+        args.file.name,
+        "tracks",
+        f"{track_id}:{out_filename}",
+    ]
+    result = subprocess.run(mkvextract_args)
+    return result
+
+
+def identify(filename: str) -> dict:
+    mkvmerge_args = ["mkvmerge", "-F", "json", "-i", filename]
+    result = subprocess.run(mkvmerge_args, stdout=subprocess.PIPE)
+    return json.loads(result.stdout)
+
+
+def filter(track: dict) -> bool:
+    return (
+        track["codec"] == "SubRip/SRT"
+        and track["properties"]["language"] in "eng"  # TODO: use language enum
+        and not track["properties"]["forced_track"]
+    )
+
+
+data = identify(args.file.name)
+tracks = [track for track in data["tracks"] if filter(track)]
+
+if not tracks:
+    print("no matching subtitle tracks found")
+    sys.exit(1)
+
+for track in tracks:
+    try:
+        print(yaml.dump(track))
+        sub_name = path.stem + ".en.srt"
+        mkvextract(track["id"], sub_name)
+        break
+    except KeyError:
+        continue
+
+sys.exit(0)
