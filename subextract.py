0d1f89a8a9f21e56ec906943d14b00e849c31be5
Salomon Popp <hi@salomonpopp.me>
2021-12-28 20:52:05 +0100

    ci: create pipeline for publishing package (#1)
    * feat: add version argument
    
    * feat: create publish pipeline

--- subextract.py
+++ subextract.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import importlib.metadata
 import json
 import logging
 import subprocess
@@ -10,6 +11,8 @@ import yaml
 
 from language import Language
 
+PACKAGE = "subextract"
+
 parser = argparse.ArgumentParser(description="Extract subtitles from mkv files")
 parser.add_argument(
     "-v",
@@ -19,6 +22,12 @@ parser.add_argument(
     dest="log_level",
     const=logging.DEBUG,
     default=logging.INFO,
+)
+parser.add_argument(
+    "-V",
+    "--version",
+    action="version",
+    version="%(prog)s " + importlib.metadata.version(PACKAGE),
 )
 group = parser.add_mutually_exclusive_group()
 group.add_argument(
