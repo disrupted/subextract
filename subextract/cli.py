2a2f0258843523adef7842469e429e4203fcf111
Salomon Popp <hi@salomonpopp.me>
2022-05-20 12:23:35 +0200

    feat: select SDH subtitles (#5)

--- subextract/cli.py
+++ subextract/cli.py
@@ -11,7 +11,7 @@ import yaml
 from . import __version__
 from .language import Language
 
-parser = argparse.ArgumentParser(description="Extract subtitles from mkv files")
+parser = argparse.ArgumentParser(description="Extract subtitles from MKV files")
 parser.add_argument(
     "-v",
     "--verbose",
@@ -27,17 +27,24 @@ parser.add_argument(
     action="version",
     version="%(prog)s " + __version__,
 )
-group = parser.add_mutually_exclusive_group()
-group.add_argument(
+parser.add_argument(
     "--id",
     type=int,
-    help="Track id to extract",
+    help="Track ID to extract",
 )
-group.add_argument(
+parser.add_argument(
     "--lang",
     type=Language,
     help="Language to extract (default: en)",
     default=Language("en"),
+)
+parser.add_argument(
+    "--sdh",
+    action="store_const",
+    help="Select hearing impaired subtitle",
+    dest="sdh",
+    const=True,
+    default=False,
 )
 parser.add_argument(
     "file",
@@ -73,6 +80,12 @@ def is_lang(track: dict, lang: Language) -> bool:
     )
 
 
+def is_sdh(track: dict, sdh: bool) -> bool:
+    return track["properties"].get("flag_hearing_impaired", False) == sdh or (
+        sdh and "SDH" in track["properties"].get("track_name", "")
+    )
+
+
 def get_out_name(path: Path, track: dict) -> str:
     lang = Language(track["properties"]["language"]).alpha1
     return f"{path.stem}.{lang}.srt"
@@ -87,10 +100,14 @@ def extract(path: Path, track: dict):
 
 def main():
     args = parser.parse_args()
+    if args.id and (args.lang or args.sdh):
+        parser.error("--id and --lang|--sdh are mutually exclusive")
+
     logging.basicConfig(
         level=args.log_level,
         format="%(levelname)s | %(message)s",
     )
+
     for f in args.file:
         path = Path(f.name)
         if path.suffix not in ".mkv":
@@ -106,7 +123,11 @@ def main():
                 logging.error(f"subtitle with id {args.id} doesn't exist")
                 sys.exit(1)
         else:
-            tracks = [t for t in data["tracks"] if is_lang(t, args.lang)]
+            tracks = [
+                t
+                for t in data["tracks"]
+                if is_lang(t, args.lang) and is_sdh(t, args.sdh)
+            ]
             if not tracks:
                 logging.warning("no matching subtitle tracks found")
                 sys.exit(1)
